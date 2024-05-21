fe4a6d63314ae9b4dfbaaa066af14bdc86699e1d
cabr3ra <uri.cabrera2000@gmail.com>
2024-05-21 15:32:33 +0200

    "Update designs for Comparator and FruitShopList and add new animation for AboutUs."

--- app/src/main/java/com/oriol/ffapp/AboutUs.kt
+++ app/src/main/java/com/oriol/ffapp/AboutUs.kt
@@ -1,11 +1,51 @@
 package com.oriol.ffapp
 
-import androidx.appcompat.app.AppCompatActivity
+
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
 
 class AboutUs : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about_us)
+
+
+        // Obtén las vistas
+        val tvTitle = findViewById<View>(R.id.tvTitle)
+        val tvDescription1 = findViewById<View>(R.id.tvDescription1)
+        val tvDescription2 = findViewById<View>(R.id.tvDescription2)
+        val tvDescription3 = findViewById<View>(R.id.tvDescription3)
+        val logoYso = findViewById<View>(R.id.logoYso)
+
+
+        // Llama a la función para animar las vistas
+        animateView(tvTitle, 0)
+        animateView(tvDescription1, 500)
+        animateView(tvDescription2, 1000)
+        animateView(tvDescription3, 1500)
+        animateView(logoYso, 2000)
+    }
+
+
+    private fun animateView(view: View, delay: Long) {
+        view.alpha = 0f
+        view.translationY = 50f
+
+
+        val animatorSet = AnimatorSet()
+        val fadeAnimator = ObjectAnimator.ofFloat(view, View.ALPHA, 0f, 1f)
+        val translationAnimator = ObjectAnimator.ofFloat(view, View.TRANSLATION_Y, 50f, 0f)
+
+
+        animatorSet.playTogether(fadeAnimator, translationAnimator)
+        animatorSet.duration = 1000
+        animatorSet.startDelay = delay
+        animatorSet.start()
     }
 }
